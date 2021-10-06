3c815b988b5ec9399d82b7e17dc03302dcface17
Usama Baig <usamabaig776@live.com>
2021-10-06 18:55:36 +0500

    Updated hero slider

--- assets/lezada.js
+++ assets/lezada.js
@@ -2,5 +2,10 @@
 $(document).ready(function () {
     $('.hero-slider').slick({
         centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        variableWidth: true,
+        nextArrow: '<button>&rsaquo;</button>',
+        prevArrow: '<button>&lsaquo;</button>',
     });
 });
